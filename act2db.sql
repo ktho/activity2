d6ec96bba055ac082a7f5994ca07052fb8354b41
ktho <ktyunho@uw.edu>
2013-05-12 23:25:30 -0700

    first commit
    main stuff

---
+++ act2db.sql
@@ -0,0 +1,177 @@
+﻿/*
+ * A database for diagramming workflow
+ */
+
+create or replace language plpgsql;
+
+DROP SCHEMA IF EXISTS act2 CASCADE;
+CREATE SCHEMA act2; 
+
+/*
+ * Represents facility
+ */
+CREATE TABLE act2.facility (
+	fid 		SERIAL PRIMARY KEY,
+	code		char(8),
+	shortName	text,
+	longName	text,
+	grossSF		int,
+	yearBuilt	int
+);
+
+/*
+ * Represents device
+ */
+CREATE TABLE act2.device (
+	did 		SERIAL PRIMARY KEY,
+	ddescription	varchar(64),
+	fid		int references act2.facility (fid) on delete no action
+);
+
+/*
+ * Represents device records with referential integrity problems
+ */
+CREATE TABLE act2.devicetemp (
+	did 		SERIAL PRIMARY KEY,
+	ddescription	varchar(64),
+	fid		int 
+);
+
+
+ /* 
+  * We create a domain to represent intervals
+  */
+ create domain act2.unit_d char(1)
+	check (value in (
+		'A',  /* 15 minutes */
+		'B',	/* 1h */ 
+		'C',	/* 4hr */ 
+		'D' 	/* 24h4 */
+)); 
+
+
+/*
+ * Represents work_log or time series data of electricity readings
+ */
+CREATE TABLE act2.work_log (
+	id 		SERIAL PRIMARY KEY,
+	did		int references act2.device (did) on delete no action,
+	timelogged	timestamp,
+	e_reading	numeric,
+	e		real,
+	delta_unit	act2.unit_d DEFAULT 'A',
+	year		int,
+	moy		int,
+	woy		int,
+	dow		int
+);
+
+
+/*
+ * Represents work_log or time series data of electricity readings
+ */
+CREATE TABLE act2.work_log_temp (
+	id 		SERIAL PRIMARY KEY,
+	did		int,
+	timelogged	timestamp,
+	e_reading	numeric,
+	e		real,
+	delta_unit	act2.unit_d DEFAULT 'A',
+	year		int,
+	moy		int,
+	woy		int,
+	dow		int
+);
+
+COPY act2.facility (fid, code, shortName, longName, grossSF, yearBuilt)
+	FROM '/var/tmp/facility.txt' WITH DELIMITER '	';
+
+COPY act2.devicetemp (did, fid, ddescription)
+	FROM '/var/tmp/device.txt' WITH DELIMITER '	';
+/*
+COPY act2.work_log_temp (id, did, timelogged, e_reading, e, delta_unit, year, moy, woy, dow)
+	FROM '/var/tmp/worklog.txt' WITH DELIMITER '	';
+*/
+
+COPY act2.work_log_temp (id, did, timelogged, e_reading, e, delta_unit, year, moy, woy, dow)
+	FROM '/var/tmp/work_log_full.txt' WITH DELIMITER '	';
+
+
+/*
+COPY act2.facility (fid, code, shortName, longName, grossSF, yearBuilt)
+	FROM '/nfs/bronfs/uwfs/dw00/d41/ktyunho/act2/facility.txt' WITH DELIMITER '	';
+
+
+COPY act2.devicetemp (did, fid, ddescription)
+	FROM '/nfs/bronfs/uwfs/dw00/d41/ktyunho/act2/device.txt' WITH DELIMITER '	';
+
+COPY act2.work_log_temp (id, did, timelogged, e_reading, e, delta_unit, year, moy, woy, dow)
+	FROM '/nfs/bronfs/uwfs/dw00/d41/ktyunho/act2/worklog.txt' WITH DELIMITER '	';
+*/
+
+	
+/*
+ * Adds data into act2.device from the temporary table devicetemp and removes legitimate records from the temptable
+ */ 
+insert into act2.device (did, ddescription, fid)
+select * from act2.devicetemp dt where dt.fid in (select f.fid from act2.facility f);
+
+delete from act2.devicetemp dt
+	where dt.fid in (select f.fid from act2.facility f);
+
+
+/*
+ * Adds data into act2.work_log from the temporary table work_log_temp and removes legitimate records from the temp table
+ */ 
+insert into act2.work_log (id, did, timelogged, e_reading, e, delta_unit, year, moy, woy, dow)
+select * from act2.work_log_temp wlt where wlt.did in (select d.did from act2.device d);
+
+delete from act2.work_log_temp wlt
+	where wlt.did in (select d.did from act2.device d);
+
+
+
+CREATE TABLE act2.log (
+	id 		SERIAL PRIMARY KEY,
+	starttime	timestamp,
+	endtime		timestamp,
+	query		text
+);
+
+
+
+CREATE OR REPLACE FUNCTION act2.logquery (
+)
+RETURNS VOID AS $PROC$	
+DECLARE 
+	st timestamp;
+	ed timestamp;
+	query text;
+	testnumber int;
+	testcount int;
+BEGIN
+	testnumber:= 10;
+
+	testcount:=0;
+
+	LOOP
+		st:= (select date_trunc('milliseconds', now()) as "start");
+
+		query:= ('select count(e), sum(e) from act2.work_log where did=143 and timelogged between ''2007-10-01 08:30:00-07'' and ''2007-10-01 09:30:00-07'';');
+		perform count(e), sum(e) from act2.work_log where did=143 and timelogged between '2007-10-01 08:30:00-07' and '2007-10-01 09:30:00-07';
+
+		ed:= (select date_trunc('milliseconds', now()) as "end");
+
+		INSERT INTO act2.log (starttime, query, endtime) values (st, query, ed);
+
+		testcount:= testcount+1;
+		
+		EXIT WHEN testcount > testnumber;
+	END LOOP;
+END;
+$PROC$ LANGUAGE plpgsql; 
+
+
+select act2.logquery();
+
+
